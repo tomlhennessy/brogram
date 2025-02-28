c92a7e0bd43388dd40aef88937c5127d5642fa5c
Tom Hennessy <tomlhennessy@outlook.com>
2025-02-28 11:01:34 +1000

    hero section

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
-export default function Layout() {
+export default function Layout(props) {
+  const { children } = props
+
+  const header = (
+    <header>
+      <h1 className='text-gradient'>The Brogram</h1>
+      <p><strong>The 30 Simple Workouts Programme</strong></p>
+    </header>
+  )
+
+  const footer = (
+    <footer>
+      <p>Built by <a href='' target='_blank'>Tom Hennessy</a></p>
+    </footer>
+  )
+
+  // https://YOUR_USERNAME.netlify.app
+
   return (
-    <></>
+    <>
+      {header}
+      {children}
+      {footer}
+    </>
   )
 }
