19f13c7612bcf802b2bf50f629ab9826c2385c9e
Tom Hennessy <tomlhennessy@outlook.com>
2025-02-28 15:00:12 +1000

    workout cards

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,7 +4,7 @@ import WorkoutCard from './WorkoutCard.jsx'
 
 export default function Grid() {
   const isLocked = false
-  const selectedWorkout = 0
+  const selectedWorkout = 4
 
   return (
     <div className='training-grid-plan'>
@@ -16,30 +16,32 @@ export default function Grid() {
         'Legs'
 
         const trainingPlan = training_plan[workoutIndex]
+        const dayNum = ((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex + 1
+        const icon = workoutIndex % 3 === 0 ? (
+          <i className='fa-solid fa-dumbbell'></i>
+        ) : (
+          workoutIndex % 3 === 1 ? (
+            <i className='fa-solid fa-weight-hanging'></i>
+          ) : (
+            <i className='fa-solid fa-bolt'></i>
+          )
+        )
 
         if (workoutIndex === selectedWorkout) {
           return (
-            <WorkoutCard key={workoutIndex} />
+            <WorkoutCard key={workoutIndex} trainingPlan={trainingPlan} type={type} workoutIndex={workoutIndex} icon={icon} dayNum={dayNum} />
           )
         }
 
         return (
           <button className={'card plan-card' + (isLocked ? 'inactive' : '')} key={workoutIndex}>
             <div className='plan-card-header'>
-              <p>Day {((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex + 1} </p>
+              <p>Day {dayNum} </p>
             </div>
             {isLocked ? (
               <i className='fa-solid fa-lock'></i>
             ) : (
-              workoutIndex % 3 === 0 ? (
-                <i className='fa-solid fa-dumbbell'></i>
-              ) : (
-                workoutIndex % 3 === 1 ? (
-                  <i className='fa-solid fa-weight-hanging'></i>
-                ) : (
-                  <i className='fa-solid fa-bolt'></i>
-                )
-              )
+              icon
             )}
             <div className='plan-card-header'>
               <h4><b>{type}</b></h4>
--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -1,7 +1,74 @@
 import React from 'react'
 
-export default function WorkoutCard() {
+export default function WorkoutCard(props) {
+  const { trainingPlan, workoutIndex, type, dayNum, icon } = props
+
+  const { warmup, workout } = trainingPlan || {}
+
   return (
-    <>sfvdgbfn</>
+    <div className='workout-container'>
+      <div className='workout-card card'>
+        <div className='plan-card-header'>
+          <p>Day {dayNum}</p>
+          {icon}
+        </div>
+        <div className='plan-card-header'>
+          <h2><b>{type} Workout</b></h2>
+        </div>
+      </div>
+
+      <div className='workout-grid'>
+        <div className='exercise-name'>
+          <h4>Warmup</h4>
+        </div>
+        <h6>Sets</h6>
+        <h6>Reps</h6>
+        <h6 className='weight-input'>Max Weight</h6>
+        {warmup.map((warmupExercise, warmupIndex) => {
+          return (
+            <React.Fragment key={warmupIndex}>
+              <div className='exercise-name'>
+                <p>{warmupIndex + 1}. {warmupExercise.name}</p>
+                <button className='help-icon'>
+                  <i className='fa-regular fa-circle-question' />
+                </button>
+              </div>
+              <p className='exercise-info'>{warmupExercise.sets}</p>
+              <p className='exercise-info'>{warmupExercise.reps}</p>
+              <input className='weight-input' placeholder='N/A' disabled />
+            </React.Fragment>
+          )
+        })}
+      </div>
+
+      <div className='workout-grid'>
+        <div className='exercise-name'>
+          <h4>Workout</h4>
+        </div>
+        <h6>Sets</h6>
+        <h6>Reps</h6>
+        <h6 className='weight-input'>Max Weight</h6>
+        {workout.map((workoutExercise, workoutIndex) => {
+          return (
+            <React.Fragment key={workoutIndex}>
+              <div className='exercise-name'>
+                <p>{workoutIndex + 1}. {workoutExercise.name}</p>
+                <button className='help-icon'>
+                  <i className='fa-regular fa-circle-question' />
+                </button>
+              </div>
+              <p className='exercise-info'>{workoutExercise.sets}</p>
+              <p className='exercise-info'>{workoutExercise.reps}</p>
+              <input className='weight-input' placeholder='14' />
+            </React.Fragment>
+          )
+        })}
+      </div>
+
+      <div className='workout-buttons'>
+        <button>Save & Exit</button>
+        <button disabled={true}>Complete</button>
+      </div>
+    </div>
   )
 }
