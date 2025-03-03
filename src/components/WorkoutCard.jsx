0d19500369c8b22a8ddd4650600116189b9b0519
Tom Hennessy <tomlhennessy@outlook.com>
2025-03-03 13:53:18 +1000

    modal

--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Modal from './Modal'
 
 export default function WorkoutCard(props) {
   const { trainingPlan, workoutIndex, type, dayNum, icon } = props
 
   const { warmup, workout } = trainingPlan || {}
 
+  const showExerciseDescription = {name: 'dbdb', description: 'wrgfojswrg'}
+
   return (
     <div className='workout-container'>
+      <Modal showExerciseDescription={showExerciseDescription} handleCloseModal={() => {}} />
       <div className='workout-card card'>
         <div className='plan-card-header'>
           <p>Day {dayNum}</p>
